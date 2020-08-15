4958e8ce1ac8bb41f0d243846664b1601623a3c6
Mumop <69701602+Mumop@users.noreply.github.com>
2020-08-14 20:24:09 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+  </div>
+</template>
+
+<script>
+  export default 
+  {
+    data(){
+
+      return{
+
+        titulo:'Céus Filmes'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
